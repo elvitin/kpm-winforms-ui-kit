0681f5675384fcec49c25376102040d584e30ce9
taveira <victortavi@outlook.com>
2022-10-04 09:21:09 -0300

    Custom TextBox added
    Custom TextBox finnaly

---
+++ CommonControls/KpmTextBox.cs
@@ -0,0 +1,167 @@
+﻿using System.ComponentModel;
+
+namespace KpmUiKit.CommonControls
+{
+  [DefaultEvent("_TextChanged")]
+  public partial class KpmTextBox : UserControl
+  {
+    //Fields
+    private Color borderColor = Color.FromArgb(153, 153, 153);
+    private Color borderFocusColor = Color.FromArgb(30, 133, 111);
+    private bool isFocused = false;
+    private int borderSize = 1;
+    private bool underlinedStyle = false;
+
+    //Events
+    public event EventHandler _TextChanged;
+
+    //Constructor
+    public KpmTextBox()
+    {
+      InitializeComponent();
+    }
+
+    //Properties
+    [Category("Kpm Custom Properties")]
+    public Color BorderFocusColor { get => borderFocusColor; set => borderFocusColor = value; }
+
+    [Category("Kpm Custom Properties")]
+    public Color BorderColor { get => borderColor; set { borderColor = value; Invalidate(); } }
+
+    [Category("Kpm Custom Properties")]
+    public int BorderSize { get => borderSize; set => borderSize = value; }
+
+    [Category("Kpm Custom Properties")]
+    public bool UnderlinedStyle { get => underlinedStyle; set { underlinedStyle = value; this.Invalidate(); } }
+
+    [Category("Kpm Custom Properties")]
+    public bool PasswordChar { get => TxtBox.UseSystemPasswordChar; set => TxtBox.UseSystemPasswordChar = value; }
+
+    [Category("Kpm Custom Properties")]
+    public bool Multiline { get => TxtBox.Multiline; set => TxtBox.Multiline = value; }
+
+    [Category("Kpm Custom Properties")]
+    public override Color BackColor
+    {
+      get => base.BackColor;
+      set
+      {
+        base.BackColor = value;
+        TxtBox.BackColor = value;
+      }
+    }
+
+    [Category("Kpm Custom Properties")]
+    public override Color ForeColor
+    {
+      get => base.ForeColor;
+      set
+      {
+        base.ForeColor = value;
+        TxtBox.ForeColor = value;
+      }
+    }
+
+    [Category("Kpm Custom Properties")]
+    public override Font Font
+    {
+      get => base.Font;
+      set
+      {
+        base.Font = value;
+        TxtBox.Font = value;
+        if (this.DesignMode)
+          UpdateControlHeight();
+      }
+    }
+
+    [Category("Custom Properties")]
+    public string Texts { get => TxtBox.Text; set => TxtBox.Text = value; }
+
+
+    //Overriden methods
+    protected override void OnPaint(PaintEventArgs e)
+    {
+      base.OnPaint(e);
+      Graphics graph = e.Graphics;
+
+      //Draw border
+      using (Pen penBorder = new(borderColor, borderSize))
+      {
+        penBorder.Alignment = System.Drawing.Drawing2D.PenAlignment.Inset;
+
+        if (isFocused)
+          penBorder.Color = BorderFocusColor;
+
+        if (underlinedStyle) //Line Style
+          graph.DrawLine(penBorder, 0, this.Height - 1, this.Width, this.Height - 1);
+        else //Normal style
+          graph.DrawRectangle(penBorder, 0, 0, this.Width - 0.5F, this.Height - 0.5F);
+      }
+    }
+
+    protected override void OnResize(EventArgs e)
+    {
+      base.OnResize(e);
+      if (this.DesignMode)
+        UpdateControlHeight();
+    }
+
+    protected override void OnLoad(EventArgs e)
+    {
+      base.OnLoad(e);
+      UpdateControlHeight();
+    }
+
+    //Private methods
+    private void UpdateControlHeight()
+    {
+      if (TxtBox.Multiline == false)
+      {
+        int txtHeight = TextRenderer.MeasureText("Text", this.Font).Height + 1;
+        TxtBox.Multiline = true;
+        TxtBox.MinimumSize = new Size(0, txtHeight);
+        TxtBox.Multiline = false;
+        this.Height = TxtBox.Height + this.Padding.Top + this.Padding.Bottom;
+      }
+    }
+
+    private void TxtBox_TextChanged(object sender, EventArgs e)
+    {
+      _TextChanged?.Invoke(sender, e);
+    }
+
+    private void TxtBox_Click(object sender, EventArgs e)
+    {
+      this.OnClick(e);
+    }
+
+    private void TxtBox_MouseEnter(object sender, EventArgs e)
+    {
+      this.OnMouseEnter(e);
+    }
+
+    private void TxtBox_MouseLeave(object sender, EventArgs e)
+    {
+      this.OnMouseLeave(e);
+    }
+
+    private void TxtBox_KeyPress(object sender, KeyPressEventArgs e)
+    {
+      this.OnKeyPress(e);
+    }
+
+    private void TxtBox_Enter(object sender, EventArgs e)
+    {
+      isFocused = true;
+      this.Invalidate();
+    }
+
+    private void TxtBox_Leave(object sender, EventArgs e)
+    {
+      isFocused = false;
+      this.Invalidate();
+    }
+    //Anexar outros eventos aqui.
+  }
+}
--- Main.Designer.cs
+++ Main.Designer.cs
@@ -28,19 +28,87 @@
     /// </summary>
     private void InitializeComponent()
     {
+      this.kpmTextBox1 = new KpmUiKit.CommonControls.KpmTextBox();
+      this.kpmTextBox2 = new KpmUiKit.CommonControls.KpmTextBox();
+      this.kpmTextBox3 = new KpmUiKit.CommonControls.KpmTextBox();
       this.SuspendLayout();
+      // 
+      // kpmTextBox1
+      // 
+      this.kpmTextBox1.BackColor = System.Drawing.SystemColors.Window;
+      this.kpmTextBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+      this.kpmTextBox1.BorderFocusColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(133)))), ((int)(((byte)(111)))));
+      this.kpmTextBox1.BorderSize = 3;
+      this.kpmTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.kpmTextBox1.ForeColor = System.Drawing.Color.DimGray;
+      this.kpmTextBox1.Location = new System.Drawing.Point(244, 83);
+      this.kpmTextBox1.Margin = new System.Windows.Forms.Padding(0);
+      this.kpmTextBox1.Multiline = false;
+      this.kpmTextBox1.Name = "kpmTextBox1";
+      this.kpmTextBox1.Padding = new System.Windows.Forms.Padding(7);
+      this.kpmTextBox1.PasswordChar = false;
+      this.kpmTextBox1.Size = new System.Drawing.Size(312, 35);
+      this.kpmTextBox1.TabIndex = 0;
+      this.kpmTextBox1.Texts = "";
+      this.kpmTextBox1.UnderlinedStyle = false;
+      // 
+      // kpmTextBox2
+      // 
+      this.kpmTextBox2.BackColor = System.Drawing.SystemColors.Window;
+      this.kpmTextBox2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+      this.kpmTextBox2.BorderFocusColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(133)))), ((int)(((byte)(111)))));
+      this.kpmTextBox2.BorderSize = 3;
+      this.kpmTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.kpmTextBox2.ForeColor = System.Drawing.Color.DimGray;
+      this.kpmTextBox2.Location = new System.Drawing.Point(315, 189);
+      this.kpmTextBox2.Margin = new System.Windows.Forms.Padding(0);
+      this.kpmTextBox2.Multiline = false;
+      this.kpmTextBox2.Name = "kpmTextBox2";
+      this.kpmTextBox2.Padding = new System.Windows.Forms.Padding(7);
+      this.kpmTextBox2.PasswordChar = false;
+      this.kpmTextBox2.Size = new System.Drawing.Size(312, 35);
+      this.kpmTextBox2.TabIndex = 1;
+      this.kpmTextBox2.Texts = "";
+      this.kpmTextBox2.UnderlinedStyle = false;
+      // 
+      // kpmTextBox3
+      // 
+      this.kpmTextBox3.BackColor = System.Drawing.SystemColors.Window;
+      this.kpmTextBox3.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+      this.kpmTextBox3.BorderFocusColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(133)))), ((int)(((byte)(111)))));
+      this.kpmTextBox3.BorderSize = 3;
+      this.kpmTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.kpmTextBox3.ForeColor = System.Drawing.Color.DimGray;
+      this.kpmTextBox3.Location = new System.Drawing.Point(412, 267);
+      this.kpmTextBox3.Margin = new System.Windows.Forms.Padding(0);
+      this.kpmTextBox3.Multiline = false;
+      this.kpmTextBox3.Name = "kpmTextBox3";
+      this.kpmTextBox3.Padding = new System.Windows.Forms.Padding(7);
+      this.kpmTextBox3.PasswordChar = false;
+      this.kpmTextBox3.Size = new System.Drawing.Size(312, 35);
+      this.kpmTextBox3.TabIndex = 2;
+      this.kpmTextBox3.Texts = "";
+      this.kpmTextBox3.UnderlinedStyle = false;
       // 
       // Main
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.SystemColors.Control;
       this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.kpmTextBox3);
+      this.Controls.Add(this.kpmTextBox2);
+      this.Controls.Add(this.kpmTextBox1);
       this.Name = "Main";
       this.Text = "Main";
       this.ResumeLayout(false);
 
     }
 
-        #endregion
-    }
+    #endregion
+
+    private CommonControls.KpmTextBox kpmTextBox1;
+    private CommonControls.KpmTextBox kpmTextBox2;
+    private CommonControls.KpmTextBox kpmTextBox3;
+  }
 }
